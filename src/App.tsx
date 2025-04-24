50cd1bc1c7404264f0f7e756db2085244d67d903
Darius Daniel <dariusdaniel771@gmail.com>
2025-04-24 18:26:57 +0100

    Built header for Desktop and moved the code to a dedicated component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import './App.css'
+import Header from "./components/header.tsx";
 
 function App() {
   return (
     <>
-      <header className="flex justify-between items-center py-[16px] px-[24px]">
-        <img
-            src="/src/assets/main%20logo.png"
-            alt="Main logo for the website"
-            className="w-[150px]"
-        />
-        <img
-          src="/src/assets/hamburger-menu.png"
-          alt=""
-          className='w-[21px]'
-        />
-      </header>
-
+      <Header />
     </>
   )
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center py-[16px] px-[24px] min-[1200px]:px-[120px] min-[1200px]:py-[24px]">
+      <img
+        src="/src/assets/main%20logo.png"
+        alt="Main logo for the website"
+        className="w-[150px]"
+      />
+      <img
+        src="/src/assets/hamburger-menu.png"
+        alt=""
+        className='w-[21px] min-[1200px]:hidden'
+      />
+      <nav>
+        <ul className="max-[1200px]:hidden flex gap-[32px] font-medium text-[20px] leading-[100%]">
+          <li>
+            <a href="#">Templates</a>
+          </li>
+          <li>
+            <a href="#">Discover</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Learn</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="max-[1200px]:hidden flex gap-[16px] font-bold text-[16px] leading-[25px]">
+        <a href="#" className="rounded-[4px] border-1 text-primary-green border-primary-green py-[16px] px-[32px]">Login</a>
+        <a href="#" className="rounded-[4px] bg-primary-green text-white py-[16px] px-[32px]">Sign up</a>
+      </div>
+    </header>
+  )
+}
