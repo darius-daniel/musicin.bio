c340360e4aea4d245192fe490a637564508cae3d
Darius Daniel <dariusdaniel771@gmail.com>
2025-04-24 17:53:48 +0100

    Initial commit to project. Already created header for mobile

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header className="flex justify-between items-center py-[16px] px-[24px]">
+        <img
+            src="/src/assets/main%20logo.png"
+            alt="Main logo for the website"
+            className="w-[150px]"
+        />
+        <img
+          src="/src/assets/hamburger-menu.png"
+          alt=""
+          className='w-[21px]'
+        />
+      </header>
+
+    </>
+  )
+}
+
+export default App
